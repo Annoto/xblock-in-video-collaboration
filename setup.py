95b6f0ef6bae59d79abc8d22208b6fcc7cacf6a3
Maxim Starodubcev <maxim@trambroid.com>
2019-03-14 12:55:22 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ import os
 from setuptools import setup
 
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+
 def package_data(pkg, roots):
     """Generic function to find package_data.
 
@@ -25,6 +29,7 @@ setup(
     name='annoto-xblock',
     version='0.1.0',
     description='An XBlock for adding Annoto in-video collaboration solution to any video',
+    long_description=README,
     license='Apache 2.0',
     packages=[
         'annoto',
