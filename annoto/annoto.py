76a666d7bcc25efccfd48239594a0968477a43fb
Henix <hen.aton@gmail.com>
2021-07-19 16:41:40 +0300

    Update annoto.py

--- annoto/annoto.py
+++ annoto/annoto.py
@@ -312,7 +312,7 @@ class AnnotoXBlock(StudioEditableXBlockMixin, XBlock):
             'exp': int(time.time() + 60 * 20),
             'iss': annoto_auth['client_id'],
             'jti': user.id,
-            'name': profile_name,
+            'name': user.username,
             'email': user.email,
             'photoUrl': photo,
             'scope': scope
